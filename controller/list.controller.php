024e0c0141b277b8302f383e9381e40ca30480c7
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-11-18 15:27:59 +0630

    3rd ver

--- controller/list.controller.php
+++ controller/list.controller.php
@@ -2,5 +2,10 @@
 function index(){
     $sql="SELECT * FROM testing";
     $query=mysqli_query($GLOBALS["con"],$sql);//php mhr global scope ka var twy ko locla scope htl mhr pyn khw tone lo m aya 
-    return view("list/index");
+    $lists=[];
+    while ($row=mysqli_fetch_assoc($query)) {
+        $lists[]=$row;
+        # code...
+    }
+    return view("list/index",["lists"=>$lists]);
 }
