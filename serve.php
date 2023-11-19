f993b9b4be82f2421cb263fa118fec92501c3fd5
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-11-19 14:28:44 +0630

    serve update

--- serve.php
+++ serve.php
@@ -1,2 +1,9 @@
 <?php 
-system("php -S localhost:8080 -t public/");
+$dynamicPort=rand(7000,9999);
+// system("php -S localhost:8080 -t public/");
+if (strtoupper(substr(PHP_OS,0,3))==="WIN") {
+    system("cd public && php -S localhost:$dynamicPort");
+}else{
+system("cd public; php -S localhost:$dynamicPort");
+}
+echo strtoupper(substr(PHP_OS,0,3));
