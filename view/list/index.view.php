fe16a74fa2402831c399b695d6a15e6e1a856824
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-11-19 14:58:35 +0630

    updated

--- view/list/index.view.php
+++ view/list/index.view.php
@@ -1,2 +1,6 @@
+
+<?php require_once ViewDir."/template/header.php" ;?>
+
 <h1>I am list index</h1>
 <?php dd($lists) ?>
+<?php require_once ViewDir."/template/footer.php" ;?>
---
+++ view/template/footer.php
@@ -0,0 +1,3 @@
+<script src="<?= url("/js/bootstrap.js")?>"></script>
+</body>
+</html>
