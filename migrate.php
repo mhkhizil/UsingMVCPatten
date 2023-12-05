c514b77872c1a0e8a3e18577835b129bbc57bf53
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-05 19:33:59 +0630

    inventory api

--- migrate.php
+++ migrate.php
@@ -4,5 +4,5 @@ require_once './core/function.php';
 deleteExistingTable();
 // createTable('testing');
 createTable("testing","name VARCHAR(50) NOT NULL","money INT(20) NOT NULL ");
-createTable("inventories","name VARCHAR(50) NOT NULL","money INT(20) NOT NULL ");
+createTable("inventories","name VARCHAR(50) NOT NULL","price INT(20) NOT NULL ","stock INT(20) NOT NULL ");
 
