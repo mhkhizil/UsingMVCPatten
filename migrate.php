b73f2014274ac02f69a070868a2bb20cdd4e7d42
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-05 19:22:27 +0630

    newApi

--- migrate.php
+++ migrate.php
@@ -4,4 +4,5 @@ require_once './core/function.php';
 deleteExistingTable();
 // createTable('testing');
 createTable("testing","name VARCHAR(50) NOT NULL","money INT(20) NOT NULL ");
+createTable("inventories","name VARCHAR(50) NOT NULL","money INT(20) NOT NULL ");
 
