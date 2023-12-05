01f26932a72b10b4cd70f7a13fde90b178438ad1
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-05 21:44:59 +0630

    update

--- view/inventory/create.view.php
+++ view/inventory/create.view.php
@@ -1,22 +1,26 @@
 <?php require_once ViewDir . "/template/header.php"; ?>
 
-<h1>Create New Lists</h1>
+<h1>Create New Items</h1>
 <div class=" d-flex  justify-content-between mb-3">
-    <a href="<?= route("list") ?>" class=" btn btn-outline-primary">All lists</a>
+    <a href="<?= route("inventory") ?>" class=" btn btn-outline-primary">All Items</a>
 </div>
 <div class="border rounded-4 p-5">
-    <form action="<?= route('list-store') ?>" method="post">
+    <form action="<?= route('inventory-store') ?>" method="post">
         <div class=" row align-items-end">
             <div class="col">
-                <label for=" form-label">Your name</label>
+                <label for=" form-label">Item name</label>
                 <input type="text" class=" form-control" name="name" required>
             </div>
             <div class="col">
-                <label for=" form-label">Your money</label>
-                <input type="number" class=" form-control" name="money" required>
+                <label for=" form-label">Price</label>
+                <input type="number" class=" form-control" name="price" required>
             </div>
             <div class="col">
-                <button class=" btn btn-primary w-100 btn-lg">Add List</button>
+                <label for=" form-label">Stock</label>
+                <input type="number" class=" form-control" name="stock" required>
+            </div>
+            <div class="col">
+                <button class=" btn btn-primary w-100 btn-lg">Add Item</button>
             </div>
         </div>
     </form>
