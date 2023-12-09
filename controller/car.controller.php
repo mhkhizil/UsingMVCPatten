e2ed88a8ebac5327df54032600dcbadbf5f2e42c
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-09 19:37:07 +0630

    update

--- controller/car.controller.php
+++ controller/car.controller.php
@@ -23,13 +23,13 @@ function store()
     // setSession("File stored successfully!");
     // dd(showSession());
     $currentUser = first("SELECT * FROM cars WHERE id={$GLOBALS['con']->insert_id}"); // connetion objects has id that i have inserted latest using sql cmd therefore i retrieve it and run sql cmd to get latest inserted user
-    return  responseJson($currentUser,201);
+    return  responseJson($currentUser, 201);
 };
 function delete()
 {
     $id = $_GET['id'];
 
-    $sql = "DELETE  FROM users WHERE id=$id";
+    $sql = "DELETE  FROM cars WHERE id=$id";
     run($sql);
     // setSession("File deleted successfully!");
     return responseJson("deleted successfully"); //server htl ka htttp referer ka nout sone twr htr dl link ko pyn po py 
@@ -39,25 +39,25 @@ function show()
     // $name = $_GET['name'];
     // $money = $_GET['money'];
     $id = $_GET['id'];
-    $sql = "SELECT * FROM users WHERE id=$id";
+    $sql = "SELECT * FROM cars WHERE id=$id";
 
     return responseJson(first($sql));
 }
 function update()
 {
-  parse_str(file_get_contents("php://input"),$_PUT);
+    parse_str(file_get_contents("php://input"), $_PUT);
     // put ka json any nk form data ka ny po lo m a ya woo formurlencoded nk mha ya ml thu ka string line g any nk yay dr 
     //ae dot ae request ka po lite dl ae formurlencoded lk htr dl data ko d phat ka pyn access lk poho ka $_POST ko tone lo ma ya woo bl lo tone ma ll so dot php://input nk phan ml br nk phan ma ll so dot file_get_content() nk phan ml br lo so dot file_get_contents ka php://input htl ka data twy ko string any nk htote py ml example name=trz&age=20 ae lo pone string ya mhr 
     //ae mhr string any nk request ka po ite dl data twy akone ya twr p ae dr ko mha parse_str() nk akone ko variable nk value talhu si htl ko htl py ml dl 
- 
+
     $name = $_PUT['name'];
-    $email = $_PUT['email'];
-    $gender = $_PUT['gender'];
-    $address = $_PUT["address"];
+    $brand = $_PUT["brand"];
+    $fuel = $_PUT['fuel'];
+    $detailSpec = $_PUT["detailSpec"];
     $id = $_GET['id'];
     // dd($_POST["id"]);
-    $sql = "UPDATE users SET sname='$name',gender='$gender',email='$email',address='$address' WHERE id=$id";
+    $sql = "UPDATE cars SET sname='$name',brand='$brand',fuel='$fuel',detailSpec='$detailSpec' WHERE id=$id";
     run($sql);
     // setSession("File updated successfully!");
-    return responseJson(first("SELECT * FROM users WHERE id=$id"));
+    return responseJson(first("SELECT * FROM cars WHERE id=$id"));
 }
